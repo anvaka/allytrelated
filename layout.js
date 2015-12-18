2e51323df7af0298864317a9f40a3701b02d833a
anvaka <anvaka@gmail.com>
2015-12-17 23:24:30 -0800

    Removed stats, uncommented required code

--- layout.js
+++ layout.js
@@ -5,9 +5,9 @@ require('./lib/loadGraph.js')(process.argv[2] || './data/youtube-user.json', sta
 function start(graph) {
   console.log('loaded graph');
   console.log(graph.getLinksCount() + ' edges; ' + graph.getNodesCount() + ' nodes');
-  // var layout = require('ngraph.offline.layout')(graph);
-  // console.log('Starting layout. This will take a while...');
-  // layout.run();
+  var layout = require('ngraph.offline.layout')(graph);
+  console.log('Starting layout. This will take a while...');
+  layout.run();
   save(graph, { outDir: './data' });
   console.log('Done.');
   console.log('Copy `links.bin`, `labels.json` and positions.bin into dat folder');
